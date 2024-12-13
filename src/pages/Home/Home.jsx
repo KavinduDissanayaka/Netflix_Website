d08d5b21b922c264c3adca5e3a1549cc4c89ded6
KavinduDissanayaka <ndkmadhuranga@std.appsc.sab.ac.lk>
2024-12-13 15:10:26 +0530

    create footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
 import TitleCard from '../../components/TitleCards/TitleCard'
+import Footer from '../../components/Footer/Footer'
 
 const Home = () => {
   return (
@@ -28,10 +29,11 @@ const Home = () => {
         </div>
         <div className="more-cards">
         <TitleCard title={"Blockbuster Movies"}/>
-        <TitleCard title={"Blockbuster Movies"}/>
-        <TitleCard title={"Blockbuster Movies"}/>
-        <TitleCard title={"Blockbuster Movies"}/>
+        <TitleCard title={"Only on Netflix"}/>
+        <TitleCard title={"Upcoming"}/>
+        <TitleCard title={"Top Pics for You"}/>
         </div>
+        <Footer/>
     </div>
   )
 }
