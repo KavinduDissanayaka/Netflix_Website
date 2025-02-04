c7f88f92924dfe5ac1221ef4978becaa0f385f5d
KavinduDissanayaka <ndkmadhuranga@std.appsc.sab.ac.lk>
2025-02-04 14:44:49 +0530

    Connect TMDB API

--- src/components/TitleCards/TitleCard.jsx
+++ src/components/TitleCards/TitleCard.jsx
@@ -1,11 +1,22 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCard.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 
 const TitleCard = ({title, category}) => {
 
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMjA0MmI3MjRmYzc3MTM5NGYxYjcyYzBmOTA1YTZkNSIsIm5iZiI6MTczODY0NTA2My40NTI5OTk4LCJzdWIiOiI2N2ExOWU0NzYwNWZhMGQ1MDAwMzAxM2MiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.2X_267_Ks6b0buqfHTtrP-p7J4EEPGt_zDpARPUXEwA'
+    }
+  };
+  
+  
 
   const handlewheel = (event)=>{
     event.preventDefault();
@@ -13,6 +24,12 @@ const TitleCard = ({title, category}) => {
   }
   
   useEffect(()=>{
+
+    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results))
+    .catch(err => console.error(err));
+
     cardsRef.current.addEventListener('wheel', handlewheel);
   },[])
   
@@ -21,10 +38,10 @@ const TitleCard = ({title, category}) => {
     <div className='titlecards'>
       <h2>{title?title:"Popular on Nerflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index)=>{
+        {apiData.map((card, index)=>{
           return <div className="card" key={index}>
-          <img src={card.image} alt=''/>
-          <p>{card.name}</p>
+          <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt=''/>
+          <p>{card.original_title}</p>
       </div>
         })}
       </div>
